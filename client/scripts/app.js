8511e86ec8b41a9078fd311c8514e33f484cf2b0
Hack Reactor Student <student@hackreactor.com>
2017-07-07 19:28:33 -0500

    initial post and get work

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,62 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages', 
+};
+app.init = function() {};
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  }); 
+};
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(response) {
+      //response is a parsed object
+      //use the attributes in this to do stuff
+      //console.log(response);
+      for (var message of response.results) {
+        //will have a username, roomname, and text attributes
+        var messageDiv = $('<div class="chat"></div>');
+        var userSpan = $('<span class="username"></span>');
+        var userText = $('<span class="user_text"></span>');
+        
+        var html = '<div class="chat"><p class="username">' + message.username + '</p><p>' + message.text + '</p></div>';
+
+        $('#chats').prepend(html);
+      }
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get messages', data);
+    }
+  }); 
+};
+/*
+$('#message_form').submit(function(){
+  $('#selected_room').val();
+  $('#message_text').val();
+});
+*/
+
+$(document).ready(function() {
+  $('#submit').click(function() {
+    app.send($('#submit').val);
+  });
+  $('#update').click(function() {
+    app.fetch();
+  });
+});
